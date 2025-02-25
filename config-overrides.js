bd9c367f5f64f717a3ff62232dace0993d2d1fc4
Chmelyuk <98876964+Chmelyuk@users.noreply.github.com>
2025-02-25 22:35:35 +0200

    Обновление проекта

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,5 @@
 module.exports = function override(config) {
-  console.log("Original rules:", config.module.rules); // Для отладки
+   // Для отладки
 
   config.module.rules = config.module.rules.map(rule => {
     // Проверяем, является ли это правилом для source-map-loader
@@ -13,6 +13,6 @@ module.exports = function override(config) {
     return rule;
   });
 
-  console.log("Modified rules:", config.module.rules); // Для отладки
+  // Для отладки
   return config;
 };
